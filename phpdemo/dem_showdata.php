07db9f1c411e1dc201d7ccfe3b3a03371d4763ed
kartiks2000 <46376692+kartiks2000@users.noreply.github.com>
2020-08-06 23:09:42 +0530

    Add files via upload

---
+++ phpdemo/dem_showdata.php
@@ -0,0 +1,35 @@
+
+
+    <?php include "db.php";
+
+                $query = "SELECT * FROM users";
+                $result = mysqli_query($connect,$query);
+
+                if(!$result){
+                    echo "sorry...";
+                }
+
+    ?>            
+
+    <html>
+        <head>
+            <title>PHP fetch data...</title>
+        </head>
+        <body>
+            <div>
+                <?php
+
+                    // while($row = mysqli_fetch_row($result)){
+                    //     print_r($row);
+                    // }
+
+                    // OR... it gives us a associative array
+
+                    while($row = mysqli_fetch_assoc($result)){
+                        print_r($row);
+                    }
+
+                ?>
+            </div>
+        </body>
+    </html>    
